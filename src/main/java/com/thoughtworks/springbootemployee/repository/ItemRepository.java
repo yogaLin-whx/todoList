28902cd83e439344f527ef541c0a19ab955ba1d7
WUCH8 <944204910@qq.com>
2020-08-06 19:08:36 +0800

    [todoList][jayden]feature:create ItemRepository

---
+++ src/main/java/com/thoughtworks/springbootemployee/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.springbootemployee.repository;
+
+import com.thoughtworks.springbootemployee.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item,Integer> {
+
+}
