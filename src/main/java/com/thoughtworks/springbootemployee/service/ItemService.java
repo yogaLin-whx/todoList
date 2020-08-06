b8125aaa868f0b324e25b8b3809b4c9d600c7056
WUCH8 <944204910@qq.com>
2020-08-06 19:11:15 +0800

    [todoList][jayden]feature:create ItemService  ImteServiceImpl

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/ItemService.java
@@ -0,0 +1,4 @@
+package com.thoughtworks.springbootemployee.service;
+
+public interface ItemService {
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/service/serviceImpl/ItemServiceImpl.java
@@ -0,0 +1,4 @@
+package com.thoughtworks.springbootemployee.service.serviceImpl;
+
+public class ItemServiceImpl {
+}
