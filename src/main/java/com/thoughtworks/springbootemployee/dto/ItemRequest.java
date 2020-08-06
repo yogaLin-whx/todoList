65237342a8170fb015042c93cc35c860e2c15e40
WUCH8 <944204910@qq.com>
2020-08-06 20:57:32 +0800

    [todoList][jayden chris]feature:create ItemRequest

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/ItemRequest.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.springbootemployee.dto;
+
+public class ItemRequest {
+
+    private String content;
+    private boolean status;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
