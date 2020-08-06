12f3d4e9c9dbc445ca14998d38c679065d0e2421
WUCH8 <944204910@qq.com>
2020-08-06 21:00:18 +0800

    [todoList][jayden chris]feature:create ItemResponse

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/ItemResponse.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.springbootemployee.dto;
+
+public class ItemResponse {
+
+    private String name;
+    private boolean status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
