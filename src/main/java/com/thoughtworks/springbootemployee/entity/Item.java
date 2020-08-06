57b7cb8d136bdf877c704b3f189cbdac84a3f4d9
WUCH8 <944204910@qq.com>
2020-08-06 19:06:07 +0800

    [todoList][chris]feature:create Item entity

---
+++ src/main/java/com/thoughtworks/springbootemployee/entity/Item.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.springbootemployee.entity;
+
+public class Item {
+
+    private int id;
+    private String content;
+    private boolean  status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
